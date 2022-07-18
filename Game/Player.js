ca358c764ad2cacbfef67662b07efb7c1fcae49e
SanjanaS7 <73141846+SanjanaS7@users.noreply.github.com>
2022-07-18 14:37:28 -0500

    Add files via upload

---
+++ Game/Player.js
@@ -0,0 +1,12 @@
+class Player extends BaseClass {
+    constructor(x,y){
+      super(x,y,50,50);
+      this.image = loadImage("Images/blob.png");
+    }
+  
+    display() {
+
+      super.display();
+
+    }
+  }
